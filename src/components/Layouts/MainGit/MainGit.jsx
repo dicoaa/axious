da7ac569a27eb5ab277092f42fee02c5af006790
Dicoaa <alexcorreadiaz@gmail.com>
2022-03-10 10:27:44 -0500

    Con Axios

--- src/components/Layouts/MainGit/MainGit.jsx
+++ src/components/Layouts/MainGit/MainGit.jsx
@@ -1,6 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const MainGit = () => {
+
+  const axios = require('axios').default;
+
+  const [data, setData] = useState({
+    form: {
+      "username":"",
+      "password":"",
+      "name":"",
+    },
+  })
+
+  async function capturarInicio (e) {
+    const capturaNombre = e.target.name
+    const capturaValor = e.target.value
+
+    if(capturaNombre === "username"){
+      setData({
+        form:{
+          ...data.form,
+          ["username"]:capturaValor
+        }
+      })
+    }
+    else if(capturaNombre === "password"){
+      setData({
+        form:{
+          ...data.form,
+          ["password"]:capturaValor
+        }
+      })
+    }
+    else if(capturaNombre === "name"){
+      setData({
+        form:{
+          ...data.form,
+          ["name"]:capturaValor
+        }
+      })
+    }
+    console.log(data.form)
+  }
+
+  const postIniciarSesion = () => {
+    // console.log(data.form)
+    axios.post('https://backend-edw.herokuapp.com/usuario', data.form)
+    .then(function (response) {
+      console.log(response);
+      if(response.data.Message === 'Usuario con ese username ya esta registrado'){
+        console.log('Usuario ya registrado')
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log('Error en la conexion')
+    });
+  }
+
+  const prevenirDefault = (e) =>{
+    e.preventDefault()
+  }
+
+
   return (
     <div className="cajonExterior">
       <div className='cajon'>
@@ -9,16 +71,16 @@ export const MainGit = () => {
 
       {/* Registrarse */}
 
-        <form onSubmit={handleOnSubmit}>
+        <form onSubmit={prevenirDefault}>
             {/* <label for="name">Nombre:</label> */}
-            <input placeholder="Nombre" type="text" id="name" name="user_name" />
+            <input placeholder="Nombre" type="text" id="name" name="username" onChange={capturarInicio}/>
             {/* <label for="password">Contrasenia:</label> */}
-            <input placeholder="Contrasenia" type="password" id="password" name="user_password" />
+            <input placeholder="Contrasenia" type="password" id="password" name="password" onChange={capturarInicio}/>
             {/* <label for="user">Usuario:</label> */}
-            <input placeholder="Usuario" type="text" id="user" name="user_user" />
+            <input placeholder="Usuario" type="text" id="user" name="name" onChange={capturarInicio}/>
             <br></br>
             <br></br>
-            <button>Registrarse</button>
+            <button onClick={postIniciarSesion}>Registrarse</button>
             <br></br>
             <a href="/Twitter">Iniciar Sesion</a>
         </form>
--- src/components/Layouts/MainTwitter/MainTwitter.jsx
+++ src/components/Layouts/MainTwitter/MainTwitter.jsx
@@ -1,23 +1,76 @@
-import React from 'react'
-import { Imagen } from '../../UI/Imagen/Imagen'
-import imgTwitter  from '../../../assets/imagenes/imgTwitter.jpg'
+import React, {useState} from 'react'
+// import axios from 'axios'
+
+// Iniciar Sesion
 
 export const MainTwitter = () => {
+
+  const axios = require('axios').default;
+
+  const [data, setData] = useState({
+    form: {
+      "username":"",
+      "password":""
+    },
+  })
+
+  async function capturarInicio (e) {
+    const capturaNombre = e.target.name
+    const capturaValor = e.target.value
+
+    if(capturaNombre === "username"){
+      setData({
+        form:{
+          ...data.form,
+          ["username"]:capturaValor
+        }
+      })
+    }
+    else if(capturaNombre === "password"){
+      setData({
+        form:{
+          ...data.form,
+          ["password"]:capturaValor
+        }
+      })
+    }
+  }
+
+  const postIniciarSesion = () => {
+
+    axios.post('https://backend-edw.herokuapp.com/login', data.form)
+    
+    .then(function (response) {
+      console.log(response);
+      if(response.data.Message === 'Credenciales Invalidas'){
+        console.log('Usuario o contrasenia incorrectos')
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log('Error en la conexion')
+    });
+  }
+
+  const prevenirDefault = (e) =>{
+    e.preventDefault()
+  }
+
   return (
     <div className="cajonExterior">
       <div className='cajon'>
         <h2>Iniciar Sesion</h2>
         <br></br>
-        <form action="/my-handling-form-page" method="post">
+        <form  onSubmit = {prevenirDefault}>
           {/* <label for="user">Usuario:</label> */}
-          <input placeholder="Usuario" type="text" id="user" name="user_user" />
+          <input placeholder="Usuario" type="text" id="username" name="username" onChange={capturarInicio}/>
 
           {/* <label for="password">Contrasenia:</label> */}
-          <input placeholder="Contrasenia" type="password" id="password" name="user_password" />
+          <input placeholder="Contrasenia" type="password" id="password" name="password" onChange={capturarInicio} />
 
           <br></br>
           <br></br>
-          <button>Iniciar Sesion</button>
+          <button onClick={postIniciarSesion}>Iniciar Sesion</button>
           <br></br>
           <a href="/Git">Nuevo usuario</a><br></br>
 
